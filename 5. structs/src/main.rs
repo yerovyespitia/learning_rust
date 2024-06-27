70b392237b4200b9962b29462b5cb53af7487751
Yerovy Espitia <luyeesgar@gmail.com>
2024-06-27 13:52:13 -0500

    example of structs & why use them

--- 5. structs/src/main.rs
+++ 5. structs/src/main.rs
@@ -1,18 +1,20 @@
-#[derive(Debug)]
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
 fn main() {
-    let user1 = User {
-        active: true,
-        username: String::from("someusername123"),
-        email: String::from("someone@example.com"),
-        sign_in_count: 1,
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
     };
 
-    println!("User1: {:?}", user1);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rect1)
+    )
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
